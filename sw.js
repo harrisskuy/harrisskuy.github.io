d459b9bbe324afa0ee4acded7c5c066ae9eab895
harrisskuy <harisudesu03@gmail.com>
2026-06-14 14:52:30 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'hris-v1';
+const assets = ['./index.html', './datalogin.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
